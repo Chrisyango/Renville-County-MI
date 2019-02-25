cf9227fabd0ec367d6399c4273d62092d03a53d5
Chris Yang <cyang287@yahoo.com>
2019-02-25 12:16:25 -0500

    finished freeform content

--- freeform.php
+++ freeform.php
@@ -1,68 +1,295 @@
 <!DOCTYPE html>
-	<html lang="en"> 
-		<head>
+<html lang="en">
+<head>
 
-			<meta charset="utf-8">
-			<meta http-equiv="X-UA-Compatible" content="IE=edge">
-			<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 
-			<title>Site Name</title>
-			<meta name="description" content="">
-			<meta name="robots" content="index, follow">
+	<title>Renville County, MN</title>
+	<meta name="description" content="">
+	<meta name="robots" content="index, follow">
 
-			<link rel="stylesheet" href="_assets_/plugins/bootstrap/css/bootstrap.min.css">	
-			<link rel="stylesheet" href="_assets_/fonts/font-awesome/css/font-awesome.min.css">
-			<link rel="stylesheet" href="_assets_/fonts/Icon-font/pe-icon-7-stroke/css/pe-icon-7-stroke.css">	
-			<link rel="stylesheet" href="_assets_/css/animate.min.css">
-			<link rel="stylesheet" href="_assets_/plugins/owl.carousel/owl-carousel/owl.carousel.css">			
-			<link rel="stylesheet" href="_assets_/css/layout.css">
+	<link rel="stylesheet" href="_assets_/plugins/bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" href="_assets_/fonts/font-awesome/css/font-awesome.min.css">
+	<link rel="stylesheet" href="_assets_/plugins/owl.carousel/owl-carousel/owl.carousel.css">
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,600i,700|Roboto:700">
+	<link rel="stylesheet" href="_assets_/css/layout.css">
 
-			<link rel="shortcut icon" href="_assets_/images/favicon.ico">
-			<link rel="apple-touch-icon" href="_assets_/images/touch-icon-iphone.png">
-			<link rel="apple-touch-icon" sizes="72x72" href="_assets_/images/touch-icon-ipad.png">
-			<link rel="apple-touch-icon" sizes="114x114" href="_assets_/images/touch-icon-iphone4.png">
-			<link rel="apple-touch-icon" sizes="144x144" href="_assets_/images/touch-icon-ipad2.png">
+	<link rel="shortcut icon" href="_assets_/images/favicon.ico">
+	<link rel="apple-touch-icon" href="_assets_/images/touch-icon-iphone.png">
+	<link rel="apple-touch-icon" sizes="72x72" href="_assets_/images/touch-icon-ipad.png">
+	<link rel="apple-touch-icon" sizes="114x114" href="_assets_/images/touch-icon-iphone4.png">
+	<link rel="apple-touch-icon" sizes="144x144" href="_assets_/images/touch-icon-ipad2.png">
 
-			<!--[if !IE]><!-->
-		 	<link rel="stylesheet" href="_assets_/plugins/add-to-homescreen/style/addtohomescreen.css">
-			<script src="_assets_/plugins/add-to-homescreen/src/addtohomescreen.min.js"></script>	
-			<script>addToHomescreen();</script>
-			<!--<![endif]-->
+	<!-- Respond.js for IE8 support of HTML5 elements and media queries -->
+	<!--[if lt IE 9]>
+	  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+	<![endif]-->
 
-			<!-- Respond.js for IE8 support of HTML5 elements and media queries -->
-			<!--[if lt IE 9]>
-			  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-			<![endif]-->
+</head>
+<body id="freeform">
 
-		</head>
-		<body id="freeform">
-		
-			<div class="main-wrap">	
-				<!-- Content Here -->
-				<!-- NOTE: Test Flyout with a tags and spans just like the main nav. Cannot assume all items are links. -->
-			</div><!-- /.main-wrap -->  
+<header class="clearfix">
+	<div id="toggles" class="hidden-lg hidden-md">
+		<label for="search-input" id="search-toggle-mobile" class="fa fa-search"></label>
+		<div id="nav-toggle" class="fa fa-bars"></div>
+	</div><!-- /#toggles -->
+
+	<a href="./" class="logo">
+		<img src="./_assets_/images/logo.jpg" alt="Logo">
+	</a>
+
+	<div id="search">
+		<form class="search-form" method="get" action="search.php">
+			<label class="fa fa-search" for="search-input"></label>
+			<input name="q" class="form-control search-input" placeholder="I'm looking for..." type="search" id="search-input">
+			<button class="fa fa-search"></button>
+		</form><!-- /.search-form -->
+	</div><!-- /#search -->
+
+	<nav>
+		<ul id="nav">
+			<li><a href="./">home</a></li>
+			<li><a href="./">about us</a></li>
+			<li>
+				<a href="./">government</a>
+				<ul>
+					<li>
+						<a href="./">section header</a>
+						<ul>
+							<li><a href="./">Ridiculus Aenean Consectetur</a></li>
+							<li><a href="./">Vehicula Elit Sollicitudin Tellus</a></li>
+							<li><a href="./">Ullamcorper Etiam Cras</a></li>
+							<li><a href="./">Fringilla Inceptos Dapibus</a></li>
+							<li><a href="./">Etiam Pharetra Fusce</a></li>
+							<li><a href="./">Vulputate Magna Etiam</a></li>
+							<li><a href="./">Egestas Quam Ornare Magna</a></li>
+							<li><a href="./">Sem Dapibus Pellentesque</a></li>
+							<li><a href="./">Fringilla Adipiscing Amet</a></li>
+							<li><a href="./">Condimentum</a></li>
+						</ul>
+					</li>
+					<li>
+						<a href="./">section header</a>
+						<ul>
+							<li><a href="./">Etiam Pharetra Fusce</a></li>
+							<li><a href="./">Vulputate Magna Etiam</a></li>
+							<li><a href="./">Egestas Quam Ornare Magna</a></li>
+						</ul>
+					</li>
+					<li>
+						<a href="./">section header</a>
+						<ul>
+							<li><a href="./">Sem Dapibus Pellentesque</a></li>
+							<li><a href="./">Fringilla Adipiscing Amet</a></li>
+							<li><a href="./">Condimentum</a></li>
+						</ul>
+					</li>
+					<li>
+						<a href="./">section header</a>
+						<ul>
+							<li><a href="./">Ridiculus Aenean Consectetur</a></li>
+							<li><a href="./">Vehicula Elit Sollictudin Tellus</a></li>
+						</ul>
+					</li>
+					<li>
+						<a href="./">section header</a>
+						<ul>
+							<li><a href="./">Fringilla Inceptos Daphibus</a></li>
+							<li><a href="./">Etiam Pharetra Fusce</a></li>
+							<li><a href="./">Vulputate Magna Etiam</a></li>
+							<li><a href="./">Egestas Quam Ornare Magna</a></li>
+							<li><a href="./">Sem Dapibus Pellentesque</a></li>
+							<li><a href="./">Fringilla Adipiscing Amet</a></li>
+						</ul>
+					</li>
+					<li>
+						<a href="./">section header</a>
+						<ul>
+							<li><a href="./">Ridiculus Aenean Consectetur</a></li>
+							<li><a href="./">Vehicula Elit Sollicitudin Tellus</a></li>
+							<li><a href="./">Ullamcorper Etiam Cras</a></li>
+							<li><a href="./">Fringilla Inceptos Dapibus</a></li>
+							<li><a href="./">Etiam Pharetra Fusce</a></li>
+							<li><a href="./">Vulputate Magna Etiam</a></li>
+							<li><a href="./">Egestas Quam Ornare Magna</a></li>
+							<li><a href="./">Sem Dapibus Pellentesque</a></li>
+							<li><a href="./">Fringilla Adipiscing Amet</a></li>
+							<li><a href="./">Condimentum</a></li>
+						</ul>
+					</li>
+				</ul>
+			</li>
+			<li><a href="./">departments</a></li>
+			<li><a href="./">services</a></li>
+			<li><a href="./">how do i...</a></li>
+		</ul> <!-- /#nav -->
+	</nav>
+</header>
+
+<section id="slider">
+	<ul class="bxslider">
+		<li style="background: url('./_assets_/images/hero.jpg') center no-repeat;background-size: cover;"></li>
+	</ul><!-- /.bxslider -->
+
+	<img src="./_assets_/images/inner-hero-whitespace.png" alt="Whitespace" id="hero-whitespace">
+</section><!-- /#slider -->
+
+<main id="main">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-3">
+				<aside id="side-content">
+					<div id="flyout-wrap">
+						<ul id="flyout">
+							<span id="flyout-header" class="header">related pages</span>
+							<li><a href="./">pellentesque aenean ipsum</a></li>
+							<li><a href="./">tellus ligula pellentesque</a></li>
+							<li>
+								<a href="./">aenean purus fusce dolor</a>
+								<ul>
+									<li><a href="./">flyout link</a></li>
+									<li><a href="./">flyout link</a></li>
+									<li><a href="./">flyout link</a></li>
+								</ul>
+							</li>
+							<li><a href="./">ullamcorpper lorem fusce</a></li>
+							<li><a href="./">ultricies porta euismod mollis</a></li>
+							<li><a href="./">vulputate sollicitudin justo</a></li>
+							<li><a href="./">cursus venenatis elit justo</a></li>
+							<li><a href="./">ligula pellentesque aenean</a></li>
+							<li><a href="./">ipsum tellus ligula</a></li>
+							<li><a href="./">pellentesque aenean purus</a></li>
+							<li><a href="./">fusce dolor ullamcorper</a></li>
+							<li><a href="./">lorem fusce ultricies porta</a></li>
+							<li><a href="./">euismod mollis vulputate</a></li>
+							<li><a href="./">sollicitudin justo cursus</a></li>
+							<li><a href="./">cenenatis elit justo ligula</a></li>
+						</ul><!-- /#flyout -->
+					</div><!-- /#flyout-wrap -->
+				</aside><!-- /#side-content -->
+			</div><!-- col-md-3 -->
+			<div class="col-md-9">
+				<article id="entry">
+					<section id="breadcrumbs">
+							<p>
+								<a href="./">
+									home
+								</a>
+								<a href="./">
+									parent page
+								</a>
+								current page
+							</p>
+					</section><!-- /#breadcrumbs -->
+					<h1 id="page-title">interior page title</h1>
+					<div id="post">
+						<p>
+							Nulla vitae elit libero, a pharetra augue. Sed posuere consectetur est at lobortis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Maecenas sed diam eget risus varius blandit sit amet non magna. Donec sed odio dui.
+						</p>
+						<img src="./_assets_/images/inner-img.jpg" alt="Image of a tractor">
+						<span class="subheader">header two</span>
+						<p>
+							Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Donec ullamcorper nulla non metus auctor fringilla. Etiam porta sem malesuada magna mollis euismod. Aenean lacinia bibendum nulla sed consectetur.
+						</p>
+						<ul>
+							<li>Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur.</li>
+							<li>Maecenas sed diam eget risus varius blandit sit amet non magna. Sed posuere consectetur est at lobortis.</li>
+						</ul>
+						<p>
+							Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Nullam quis risus eget urna mollis ornare vel eu leo. Vestibulum id ligula porta felis euismod semper.
+						</p>
+						<p>
+							Maecenas faucibus mollis interdum. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam id dolor id nibh ultricies vehicula ut id elit. Donec sed odio dui. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Donec sed odio dui.
+						</p>
+					</div><!-- /#post -->
+				</article><!-- /#entry -->
+			</div><!-- /.col-md-9 -->
+		</div><!-- /.row -->
+	</div><!-- /.container -->
+</main>
+
+<footer>
+	<div class="container">
+		<div id="footer-left">
+			<span>&copy; 2019 Renville County, MN</span> <a href="./">contact us</a>
+		</div><!-- /#footer-left -->
+		<div id="footer-right">
+			<span>Powered by <a href="https://www.revize.com/" id="revize-logo">revize.</a>, The Government Website Experts</span> <a href="./">login</a> 
+		</div> <!-- /#footer-right -->
+	</div>
+</footer>
+
+<div class="alert alert-dismissible floating-alert fade in os-animation" data-os-animation="fadeInUpBig" data-os-animation-delay="2s" role="alert">
+	<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+	<h1>Alert Title 30 characters Max</h1>
+	<p>Max 120 characters Max 120 characters Max 120 characters Max 120 characters Max 120 characters Max 120 characters Max 120</p>
+	<p><a href="#" class="btn">Read More</a></p>
+</div><!-- /.alert -->
+
+
+<!-- Share widget make into an include file -->
+<button type="button" class="share-btn floating-share-btn" data-toggle="modal" data-target="#shareModal">
+	<i class="fa fa-share-alt"></i>
+	<span>Share</span>
+</button>
+
+<div class="modal fade" id="shareModal" tabindex="-1" role="dialog" aria-labelledby="shareModal">
+	<div class="modal-dialog modal-lg" role="document">
+		<div class="modal-content">
+			<div class="modal-header">
+				<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+				<h4 class="modal-title" id="myModalLabel">Share this page</h4>
+			</div>
+			<div class="modal-body">
+				<div class="copylink">
+					<p>Copy and paste this code into your website.</p>
+					<pre>&lt;a href="<?php echo 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']; ?>">Your Link Name&lt;/a&gt;</pre>
+				</div><!-- /.copylink -->
+				<div class="share-btns">
+					<p>Share this page on your favorite Social network</p>
+					<div class="row">
+						<div class="col-md-3 col-xs-6">
+							<a href="https://www.facebook.com/sharer/sharer.php?u=<?php echo 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']; ?>" class="btn-facebook" onclick="return !window.open(this.href, 'facebook ', 'width=500,height=500')"
+							target="_blank">
+								<i class="fa fa-facebook"></i> Facebook
+							</a>
+						</div>
+						<div class="col-md-3 col-xs-6">
+							<a href="https://www.twitter.com/home?status=<?php echo 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']; ?>" class="btn-twitter" onclick="return !window.open(this.href, 'twitter ', 'width=500,height=500')"
+							target="_blank">
+								<i class="fa fa-twitter"></i> Twitter
+							</a>
+						</div>
+						<div class="col-md-3 col-xs-6">
+							<a href="https://www.plus.google.com/share?url=<?php echo 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']; ?>" class="btn-google" onclick="return !window.open(this.href, 'google ', 'width=500,height=500')"
+							target="_blank">
+								<i class="fa fa-google-plus"></i> Google Plus
+							</a>
+						</div>
+						<div class="col-md-3 col-xs-6">
+							<a href="https://www.reddit.com/submit?url=<?php echo 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']; ?>" class="btn-reddit" onclick="return !window.open(this.href, 'redit ', 'width=500,height=500')"
+							target="_blank">
+								<i class="fa fa-reddit"></i> Reddit
+							</a>
+						</div>
+					</div><!-- /.row -->
+				</div><!-- /.share-btns -->
+				<button type="button" class="btn btn-success btn-lg" data-dismiss="modal">Close</button>
+			</div><!-- /.modal-body -->
+		</div>
+	</div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
+<!-- Share widget make into an include file -->
+
 
 <script src="_assets_/js/jquery.min.js"></script>
 <script src="_assets_/plugins/modernizr/modernizr.custom.js"></script>
 <script src="_assets_/plugins/owl.carousel/owl-carousel/owl.carousel.min.js"></script>
 <script src="_assets_/plugins/jquery.bxslider/jquery.bxslider.min.js"></script>
 <script src="_assets_/plugins/bootstrap/js/bootstrap.min.js"></script>
-<script src="_assets_/plugins/twitter/js/jquery.tweet.min.js"></script>
-<script src="_assets_/plugins/instafeed/instafeed.min.js"></script>
-<script src="_assets_/plugins/sticky/jquery.sticky.js"></script>
-<script src="_assets_/plugins/matchHeight/dist/jquery.matchHeight-min.js"></script>
-<script src="_assets_/plugins/revizeWeather/js/revizeWeather.min.js"></script>
-<script src="_assets_/plugins/waypoints/waypoints.min.js"></script>
 <script src="_assets_/js/scripts.js"></script>
-<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-5113ea471e44f9dd"></script>
-<script type="text/javascript">
-  addthis.layers({
-    'theme' : 'transparent',
-    'share' : {
-      'position' : 'right',
-      'numPreferredServices' : 6
-    } 
-  });
-</script>
+
 </body>
 </html>
